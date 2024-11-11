43cf62e999911039d58881c535f0e42d7c9b5ed4
c-abhijith <cabhijithofficial@gmail.com>
2024-11-11 22:57:22 +0530

    updaed new projects

--- src/components/Project/ProjectData.jsx
+++ src/components/Project/ProjectData.jsx
@@ -1,10 +1,10 @@
 // src/projectsData.js
 const projectsData = [
     {
-        "id": 0,
+        "id": 1,
         "title": "Todo App",
         "keys": ["React", "Bootstrap"],
-        "description": "This is a Flask template project that integrates PostgreSQL for backend operations, with dynamic image rendering of a 'draw' image. It uses Flask templates to serve content efficiently while managing user data through a PostgreSQL database.",
+        "description": "The React To-Do app allows users to add tasks that are temporarily stored, providing an interactive way to manage their to-do list, but the data is not persisted beyond the current session.",
         "category": ["Python","Web"],
         "image": `${process.env.PUBLIC_URL}/assets/todo.png`,
         "git": "https://github.com/c-abhijith/Todo-APP.git",
@@ -21,10 +21,21 @@ const projectsData = [
         "git": "https://github.com/c-abhijith/Galary.git",
         "web": "https://draw-2gdntp5tw-abhis-projects-a823021e.vercel.app/",
         "video":""
+    }, 
+    {
+        "id": 2,
+        "title": "Chat",
+        "keys": ["Django", "JS", "WebSockets"],
+        "description": "The Django chat room application uses WebSockets for real-time communication, allowing users to join chat rooms, send and receive messages instantly. The template dynamically updates the chat interface as messages are exchanged, creating an interactive and responsive chat experience.",
+        "category": ["Python","Web"],
+        "image": `${process.env.PUBLIC_URL}/assets/chat.png`,
+        "git": "https://github.com/c-abhijith/Chat_App-DTL-.git",
+        "web": "https://chat-app-dtl.onrender.com/",
+        "video":""
     }
     ,
     {
-        "id": 2,
+        "id": 3,
         "title": "Ogle",
         "keys": ["Python", "Django", "SQL"],
         "description": "In my latest e-commerce project, I created a platform with Admin and User roles, secure OTP validation, and reporting in Excel and PDF formats. The system also integrates PayPal for seamless transactions, demonstrating my ability to deliver complex, user-centric solutions with robust security and reporting features.",
@@ -34,7 +45,7 @@ const projectsData = [
         "web": "https://youtu.be/Q1fKqazATtc?si=Xnjk9hE0LGP9P_SE"
     },
     {
-        "id": 3,
+        "id": 4,
         "title": "Zwast",
         "keys": ["React", "Django", "Postgres"],
         "description": "In my waste management project, I built a platform with React and Django REST Framework (DRF), featuring JWT authentication, interactive React maps, and payment options via PayPal and Razorpay. This project showcases my skills in integrating modern technologies for improved functionality and user experience.",
@@ -44,7 +55,7 @@ const projectsData = [
         "web": "https://youtu.be/ndbB3nIJwH8?si=KE_164j6IO_J7u-C"
     },
     {
-        "id": 4,
+        "id": 5,
         "title": "Netflix",
         "keys": ["React"],
         "description": "In my Netflix project, I built a platform with React, featuring video streaming functionality and an integrated API. This project highlights my skills in creating a seamless and interactive user experience using modern web technologies.",
@@ -54,7 +65,7 @@ const projectsData = [
         "web": ""
     },
     {
-        "id": 5,
+        "id": 6,
         "title": "OLX",
         "keys": ["React", "Firebase"],
         "description": "In my OLX-like project, I used React and Firebase to create a platform for buying and selling items. The project demonstrates my proficiency in integrating front-end and back-end technologies to build a functional and user-friendly application.",
@@ -64,7 +75,7 @@ const projectsData = [
         "web": ""
     },
     {
-        "id": 6,
+        "id": 7,
         "title": "KTM",
         "keys": ["HTML", "CSS"],
         "description": "The KTM project showcases a website built with HTML and CSS. It highlights my ability to create visually appealing and responsive designs with fundamental web technologies.",
