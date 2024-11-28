6a934a651ec4b3f63cd4143f3903037209e26837
AbhijithSyoft <abhijith@syoft.com>
2024-11-29 02:03:12 +0530

    updated

--- src/components/Qulifications/Qulification.jsx
+++ src/components/Qulifications/Qulification.jsx
@@ -10,7 +10,7 @@ export default function Qualification() {
 
     return (
         <section className="qualification section" id="qualification">
-            <h2 className="section__title">My Journey</h2>
+            <h2 className="section__title">Journey</h2>
 
             <div className="qualification__container">
                 <div className="qualification__tabs">
@@ -34,44 +34,40 @@ export default function Qualification() {
                 <div className="qualification__sections">
                     {/* Education Section */}
                     <div className={`qualification__content ${toggleState === 1 ? 'active-content' : ''}`}>
-                        <div className="steps">
-                            <div className="steps__item">
-                                <div className="steps__content">
-                                    <div className="steps__year">2024 - Present</div>
-                                    <div className="steps__details">
-                                        <h3>MCA</h3>
-                                        <p>JAIN (Kochi)</p>
-                                    </div>
+                        <div className="timeline">
+                            <div className="timeline__item left">
+                                <div className="timeline__content">
+                                    <div className="timeline__period">2024 - Present</div>
+                                    <div className="timeline__marker"></div>
+                                    <h3>MCA</h3>
+                                    <p>JAIN (Kochi)</p>
                                 </div>
                             </div>
 
-                            <div className="steps__item">
-                                <div className="steps__content">
-                                    <div className="steps__year">2017 - 2020</div>
-                                    <div className="steps__details">
-                                        <h3>BCA</h3>
-                                        <p>University of Kerala</p>
-                                    </div>
+                            <div className="timeline__item right">
+                                <div className="timeline__content">
+                                    <div className="timeline__period">2017 - 2020</div>
+                                    <div className="timeline__marker"></div>
+                                    <h3>BCA</h3>
+                                    <p>University of Kerala</p>
                                 </div>
                             </div>
 
-                            <div className="steps__item">
-                                <div className="steps__content">
-                                    <div className="steps__year">2015 - 2017</div>
-                                    <div className="steps__details">
-                                        <h3>Computer Science</h3>
-                                        <p>KPMHSS</p>
-                                    </div>
+                            <div className="timeline__item left">
+                                <div className="timeline__content">
+                                    <div className="timeline__period">2015 - 2017</div>
+                                    <div className="timeline__marker"></div>
+                                    <h3>Computer Science</h3>
+                                    <p>KPMHSS</p>
                                 </div>
                             </div>
 
-                            <div className="steps__item">
-                                <div className="steps__content">
-                                    <div className="steps__year">2015</div>
-                                    <div className="steps__details">
-                                        <h3>SSLC</h3>
-                                        <p>GHS Pooyappally</p>
-                                    </div>
+                            <div className="timeline__item right">
+                                <div className="timeline__content">
+                                    <div className="timeline__period">2015</div>
+                                    <div className="timeline__marker"></div>
+                                    <h3>SSLC</h3>
+                                    <p>GHS Pooyappally</p>
                                 </div>
                             </div>
                         </div>
@@ -79,34 +75,31 @@ export default function Qualification() {
 
                     {/* Experience Section */}
                     <div className={`qualification__content ${toggleState === 2 ? 'active-content' : ''}`}>
-                        <div className="steps">
-                            <div className="steps__item">
-                                <div className="steps__content">
-                                    <div className="steps__year">2022 - 2023</div>
-                                    <div className="steps__details">
-                                        <h3>Jr Software Developer</h3>
-                                        <p>Syoft</p>
-                                    </div>
+                        <div className="timeline">
+                            <div className="timeline__item left">
+                                <div className="timeline__content">
+                                    <div className="timeline__period">2022 - 2023</div>
+                                    <div className="timeline__marker"></div>
+                                    <h3>Jr Software Developer</h3>
+                                    <p>Syoft</p>
                                 </div>
                             </div>
 
-                            <div className="steps__item">
-                                <div className="steps__content">
-                                    <div className="steps__year">2021 - 2022</div>
-                                    <div className="steps__details">
-                                        <h3>Software Developer</h3>
-                                        <p>CYBERIA (TVM)</p>
-                                    </div>
+                            <div className="timeline__item right">
+                                <div className="timeline__content">
+                                    <div className="timeline__period">2021 - 2022</div>
+                                    <div className="timeline__marker"></div>
+                                    <h3>Software Developer</h3>
+                                    <p>CYBERIA (TVM)</p>
                                 </div>
                             </div>
 
-                            <div className="steps__item">
-                                <div className="steps__content">
-                                    <div className="steps__year">2020 - 2021</div>
-                                    <div className="steps__details">
-                                        <h3>Intern</h3>
-                                        <p>CYBERIA (TVM)</p>
-                                    </div>
+                            <div className="timeline__item left">
+                                <div className="timeline__content">
+                                    <div className="timeline__period">2020 - 2021</div>
+                                    <div className="timeline__marker"></div>
+                                    <h3>Intern</h3>
+                                    <p>CYBERIA (TVM)</p>
                                 </div>
                             </div>
                         </div>
