77e5c4f6ab53b0fdb488b92397e07f756ded44b9
AbhijithSyoft <abhijith@syoft.com>
2024-11-30 18:04:08 +0530

    updaed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,9 +8,6 @@ export default function Home() {
   return (
     <section className="home" id="home">
       <div className="home__container">
-        {/* Single Resume button fixed at the top right */}
-        <div className="home__resume">
-        </div>
         <div className="home__content">
           <Data />
         </div>
